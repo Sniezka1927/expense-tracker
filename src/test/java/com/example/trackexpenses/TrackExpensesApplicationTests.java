1bbd71fe518dd283b33069b1b0031f670761ef6c
Aleksander <dygons14@gmail.com>
2025-05-20 22:06:50 +0200

    Initialize repository with database connection

---
+++ src/test/java/com/example/trackexpenses/TrackExpensesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trackexpenses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackExpensesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
